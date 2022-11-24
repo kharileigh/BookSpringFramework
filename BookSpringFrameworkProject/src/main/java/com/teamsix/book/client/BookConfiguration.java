7d1e9cb7f34c588dc276c548924c9357526e8ba8
Leah <95726193+1eahjk@users.noreply.github.com>
2022-11-24 11:34:23 +0000

    Group 6

--- BookSpringFrameworkProject/src/main/java/com/teamsix/book/client/BookConfiguration.java
+++ BookSpringFrameworkProject/src/main/java/com/teamsix/book/client/BookConfiguration.java
@@ -10,28 +10,29 @@ import com.teamsix.book.persistence.BookDaoImpl;
 import com.teamsix.book.presentation.BookPresentationImpl;
 import com.teamsix.book.service.BookServiceImpl;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 // TESTING PROJECT
 @Configuration
-public class BookConfiguration {
-    
-    @Bean(name="dao")
-    public BookDaoImpl getPersistence() {
-            return new BookDaoImpl();
-    }
-
-    @Bean(name="service")
-    public BookServiceImpl getBookService() {
-            //Constructor Injection
-            return new BookServiceImpl(getPersistence());
-    }
-
-    @Bean(name="bkPresentation")
-    public BookPresentationImpl getBookPresentation() {
-            //Setter Injection
-            BookPresentationImpl presentation=new BookPresentationImpl();
-            presentation.setBookService(getBookService());
-            return presentation;
-    }
+	@ComponentScan(basePackages = "com.sujata")
+	public class BookConfiguration {
+	
+	@Bean(name="MySqlDataSource")
+	public DriverManagerDataSource getDataSource() {
+		DriverManagerDataSource ds=new DriverManagerDataSource();
+		ds.setDriverClassName("org.springframework.jdbc.datasource.DriverManagerDataSource");
+		ds.setUrl("jdbc:mysql://127.0.0.1:3306/wileydi001");
+		ds.setUsername("root");
+		ds.setPassword("rootroot");
+		
+		return ds;
+	}
+	
+	@Bean
+	public JdbcTemplate getTemplate() {
+		return new JdbcTemplate(getDataSource());
+	}
 }
